71c3e62b1ff727d9e72751525bea592d82cffb07
Caio Diego <caiodsj96@gmail.com>
2023-08-08 11:01:41 -0300

    resolução bug botao voltar ao topo pag chefs

--- js/script4.js
+++ js/script4.js
@@ -1,8 +1,13 @@
 window.addEventListener('scroll', function() {
   var button = document.getElementById('btn-top');
-  if (window.scrollY > 100) {
+  if (window.scrollY > 250) {
     button.style.display = 'block';
   } else {
     button.style.display = 'none';
   }
 });
+
+document.getElementById('btn-top').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
