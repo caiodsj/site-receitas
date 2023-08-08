4eb5531181a87284591fbd8786723c699db5cb63
Caio Diego <caiodsj96@gmail.com>
2023-08-08 10:57:44 -0300

    ajuste da responsividade home page

--- js/script1.js
+++ js/script1.js
@@ -7,7 +7,7 @@ btn.addEventListener("click", ()=>{
 
 window.addEventListener('scroll', function() {
     var button = document.getElementById('btn-top');
-    if (window.scrollY > 100) {
+    if (window.scrollY > 250) {
       button.style.display = 'block';
     } else {
       button.style.display = 'none';
